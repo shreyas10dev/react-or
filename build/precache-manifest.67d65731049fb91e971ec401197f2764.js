88bc44cb94ed57c9f38b840544360492bfa1f747
Shreyas Tendulkar <shreyas@atollogy.com>
2019-07-04 19:00:55 -0700

    Creating first app

---
+++ build/precache-manifest.67d65731049fb91e971ec401197f2764.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9697664d10abc4fee77374ef14efec52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32a5441ce56369062458",
+    "url": "/static/js/2.1f37c9fa.chunk.js"
+  },
+  {
+    "revision": "595b0c57e167fa478a91",
+    "url": "/static/js/main.db39cd98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
